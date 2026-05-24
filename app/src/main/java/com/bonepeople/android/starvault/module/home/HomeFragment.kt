b0ab26ee2b0bf39c8716e0e6b98a10275533974a
bonepeople <bonepeople@qq.com>
2026-05-24 23:49:46 +0800

    在 HomeFragment 添加创建与打开按钮

--- app/src/main/java/com/bonepeople/android/starvault/module/home/HomeFragment.kt
+++ app/src/main/java/com/bonepeople/android/starvault/module/home/HomeFragment.kt
@@ -4,11 +4,14 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.safeDrawing
 import androidx.compose.foundation.layout.windowInsetsPadding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -16,7 +19,9 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.ComposeView
 import androidx.compose.ui.platform.ViewCompositionStrategy
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.fragment.app.Fragment
+import com.bonepeople.android.widget.util.AppToast
 
 class HomeFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
@@ -34,10 +39,25 @@ class HomeFragment : Fragment() {
                 .fillMaxSize()
                 .windowInsetsPadding(WindowInsets.safeDrawing)
         ) {
-            Text(
-                text = "HomeFragment",
-                modifier = Modifier.align(Alignment.Center)
-            )
+            Column(
+                modifier = Modifier.align(Alignment.Center),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.spacedBy(12.dp)
+            ) {
+                Text(text = "HomeFragment")
+                Button(
+                    onClick = {
+                        AppToast.show("施工中")
+                    }) {
+                    Text(text = "创建")
+                }
+                Button(
+                    onClick = {
+                        AppToast.show("施工中")
+                    }) {
+                    Text(text = "打开")
+                }
+            }
         }
     }
 }
